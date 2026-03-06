938d6a89d74334a65a266c9669dc0e3a5a111293
MarioDJ317 <mjohnson370@mail.valenciacollege.edu>
2026-03-05 23:48:22 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function videoFunction(){
+    document.getElementById("myComment").innerHTML += "A great vacation video";
+}
+function emailMessage(){
+    document.getElementById("emailBox").innerHTML+="  Please enter valid email address";
+}
+function blankMessage(){
+    document.getElementById("emailBox").innerHTML="Email: ";
+}
